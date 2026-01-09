75446466a16544e6fa97f8886db522068c427e66
edgfb-169 <fuk4ug6c@800334.xyz>
2026-01-09 15:57:59 +0800

    Add ESLint config, workflow, and example with lint errors

---
+++ src/example.js
@@ -0,0 +1,10 @@
+// This file has intentional linting errors
+const unusedVar = 'this is unused';
+
+function example() {
+  console.log("This uses double quotes");
+  let x = 5
+  return x
+}
+
+example();
